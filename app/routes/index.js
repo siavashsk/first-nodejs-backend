6bd463411380375cfee15f1a97b4d7e69bfe4095
siavash <siavash.khani200@gmail.com>
2023-10-27 20:01:02 +0330

    feat(productController): product are now available

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,10 @@
 const userRouter = require("./users");
+const productRouter = require("./products");
 const sessionRouter = require("./sessions");
 const auth = require("../middlewares/auth");
 
 module.exports = (app) => {
-  app.use("/api/v1/users", userRouter);
-  app.use("/api/v1/session", sessionRouter);
+  app.use("/api/v1/users", [auth], userRouter);
+  app.use("/api/v1/products", productRouter);
+  app.use("/api/v1/session", [auth], sessionRouter);
 };
