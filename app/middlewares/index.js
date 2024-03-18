1cd86e32746deb0ec632e0ed37f9766eddc3c40f
siavash <siavash.khani200@gmail.com>
2024-03-19 00:36:45 +0330

    feat(middleware): added express validator middleware

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -1,6 +1,9 @@
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const expressValidator = require("express-validator");
+
 module.exports = (app) => {
   app.use(cors());
   app.use(bodyParser.json());
+  app.use(expressValidator());
 };
